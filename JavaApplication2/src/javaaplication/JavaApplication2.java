86b1e4aec43d31c74dfbe8825e98f061a393a397
class <class@CST_N417_05.cst.citytech.cuny.edu>
2015-05-13 16:40:07 -0400

    formatted

--- JavaApplication2/src/javaaplication/JavaApplication2.java
+++ JavaApplication2/src/javaaplication/JavaApplication2.java
@@ -5,39 +5,37 @@ import java.util.Scanner;
 /**
  *
  * @author Aleksandra M.
-*/
-public class JavaApplication2{
+ */
+public class JavaApplication2 {
 
- public static void main(String[] args) {
- double sum = 0.0;
- double sd = 0.0;
+    public static void main(String[] args) {
+        double sum = 0.0;
+        double sd = 0.0;
 
- Scanner keyboard = new Scanner(System.in);
- System.out.print("How many students are in class? ");
- int numStudents = keyboard.nextInt();
- int[] students = new int[numStudents];
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("How many students are in class? ");
+        int numStudents = keyboard.nextInt();
+        int[] students = new int[numStudents];
 
- for (int i = 0; i < numStudents; i++) {
- System.out.print("Enter the grade for each of the students: ");
- students[i] = (int) keyboard.nextDouble();
+        for (int i = 0; i < numStudents; i++) {
+            System.out.print("Enter the grade for each of the students: ");
+            students[i] = (int) keyboard.nextDouble();
 
- sum = sum + students[i];
- }
+            sum = sum + students[i];
+        }
 
-System.out.println();
+        System.out.println();
 
- double average = sum / students.length;
- System.out.println("The class average is: " + average);
+        double average = sum / students.length;
+        System.out.println("The class average is: " + average);
 
- for (int i = 0; i < numStudents; i++) {
- sd += ((students[i] - average) * (students[i] - average)) / (students.length - 1);
- }
+        for (int i = 0; i < numStudents; i++) {
+            sd += ((students[i] - average) * (students[i] - average)) / (students.length - 1);
+        }
 
- double standardDev = Math.sqrt(sd);
- System.out.println("Standard Deviation is: " + standardDev);
+        double standardDev = Math.sqrt(sd);
+        System.out.println("Standard Deviation is: " + standardDev);
 
- System.out.println();
- }
-  }
-    
-
+        System.out.println();
+    }
+}
