644d07ea22faabfb3d494fd741590696d00adaac
class <class@CST_N417_04.cst.citytech.cuny.edu>
2015-05-13 16:56:22 -0400

    Done ArrayHomework

---
+++ ArrayHomework/src/arrayhomework/ArrayHomework.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayhomework;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sameen Qaiser
+ */
+public class ArrayHomework {
+
+        public static void main(String[] args) {
+            double sum = 0.0;
+            double sd = 0.0;
+
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("How many students are there? ");
+            int numStudents = keyboard.nextInt();
+            int[] students = new int[numStudents];
+
+            for (int i = 0; i < numStudents; i++) {
+                System.out.print("Enter the grade for each student: ");
+                students[i] = (int) keyboard.nextDouble();
+
+                sum = sum + students[i];
+            }
+
+            System.out.println();
+
+            double average = sum / students.length;
+            System.out.println("Class average: " + average);
+
+            for (int i = 0; i < numStudents; i++) {
+                sd += ((students[i] - average) * (students[i] - average)) / (students.length - 1);
+            }
+
+            double standardDev = Math.sqrt(sd);
+            System.out.println("Standard Deviation: " + standardDev);
+
+            System.out.println();
+        }
+    }
+
+}
